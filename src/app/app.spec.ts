7c88c6ff9464732986db7b3767ad29842695b816
Geovanny Neves Liberato <gnliberato@outlook.com>
2026-01-07 21:49:54 -0300

    Criado o primeiro componente

--- src/app/app.spec.ts
+++ src/app/app.spec.ts
@@ -18,6 +18,6 @@ describe('App', () => {
     const fixture = TestBed.createComponent(App);
     await fixture.whenStable();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, modern-angular');
+    expect(compiled.querySelector('p')?.textContent).toContain('This is my first Angular component!');
   });
 });
